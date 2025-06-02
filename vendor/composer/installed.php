f66a29c165dba82b07ebee53a687e7acf3dd434d
CodeSolz <info@codesolz.net>
2025-06-02 04:22:50 +0000

    security patch updated

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'codesolz/ultimate-push-notifications',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '3d6d2263915bbbad440fbaf8ca8b7bcc80e92c02',
+        'reference' => 'a5c835bab15cc06162148b5d3f1733fbf4c95d85',
         'type' => 'composer-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'codesolz/ultimate-push-notifications' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '3d6d2263915bbbad440fbaf8ca8b7bcc80e92c02',
+            'reference' => 'a5c835bab15cc06162148b5d3f1733fbf4c95d85',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
