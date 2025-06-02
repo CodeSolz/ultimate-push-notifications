70abfffc643a1bc87c2c517be65dbac75098a64c
CodeSolz <info@codesolz.net>
2025-06-02 03:40:41 +0000

    patch updated

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codesolz/ultimate-push-notifications',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e2693f59dfd751c16656c018435e930bd708fbcf',
+        'type' => 'composer-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codesolz/ultimate-push-notifications' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e2693f59dfd751c16656c018435e930bd708fbcf',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
